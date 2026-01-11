de8ec29006b19d9c4ba2624c33c7f46cb165c685
Alexandre de Oliveira <aleloliv555@hotmail.com>
2026-01-10 23:25:13 -0300

    week 1 code assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // -----------------------------------------------------------------------------------------------------------------
+        // First create an empty array of type double and size using the variable length and an variable which value is 0. 
+        // Then create a loop that will use an temporary int variable that will count
+        // from 1.0 to the length and multiply the number variable by the temporary variable and add the result to the array,
+        // the resulting array should be returned at the end.
+        // ----------------------------------------------------------------------------------------------------------------
+        double[] multiples = new double[length]; // Empty array with size of length
+        double multiple = 0.0; // Variable which will hold the value to be added to the multiples[] array
+        for (int i = 1; i <= length; i++) // the i variable will start from 1, because it will mutiply the number by it, so it can't be 0.
+        {
+            multiple = number * i;
+            multiples[i-1] = multiple; // The i variable must be subtracted by 1, because the first position in the array is 0.
+        }
 
-        return []; // replace this return statement with your own
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +42,32 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // -----------------------------------------------------------------------------------------------------------------
+        // First, create a new temporary list that contains the values from the amount variable to the end of the original list, 
+        // than remove the values from the original list,
+        // lastly insert the values to the end of the original list, thus manipulating the original list and returning nothing.
+        // -----------------------------------------------------------------------------------------------------------------
+        if (data.Count == 0) // This makes sure that the list is not empty.
+        {
+            return;
+        }
+
+        amount %= data.Count; // This is in case the amount value is greater than the list size.
+
+        if (amount == 0) // This makes sure that the amount is not 0.
+        {
+            return;
+        }
+        
+        var tempData = data.GetRange(0, data.Count - amount); // This is the temporary list that receives the values from the original list from the begining to the number of values - amount index.
+        
+        for (int i = 0; i < tempData.Count; i++) // Removes from the original list 
+        {
+            data.RemoveAt(0);
+        }
+        foreach (var number in tempData)
+        {
+            data.Add(number);
+        }
     }
 }
